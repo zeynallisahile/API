a129cb0319c9304fa3089f4153b3bf9e029365ec
zeynallisahile <99805984+zeynallisahile@users.noreply.github.com>
2022-06-26 12:55:04 +0400

    Add files via upload

---
+++ API project1/API project1/Controllers/ActorController.cs
@@ -0,0 +1,48 @@
+﻿using API_project1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API_project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+        private List<Actor> _actors;
+        public ActorController()
+        {
+
+            _actors = new List<Actor>
+            {
+                new Actor
+                {
+                    Id = 1,
+                    FullName ="Tom Hardy",
+                    Image =""
+                },
+
+                new Actor
+                {
+                    Id = 2,
+                    FullName = "Jason Statham",
+                    Image = ""
+                },
+
+                new Actor
+                {
+                    Id = 3,
+                    FullName = "Sylvester Stallone",
+                    Image = ""
+                }
+
+            };
+
+        }
+            public IActionResult All()
+            {
+                return Ok(_actors);
+            }
+       
+    }
+}
---
+++ API project1/API project1/Models/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace API_project1.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Image { get; set; }
+    }
+}
